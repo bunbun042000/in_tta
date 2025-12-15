c5e7e5abb47eb3ac454b079f312020139a0f3068
Yamagata Fumihiro <bunbun042000@gmail.com>
2025-12-15 22:48:28 +0900

    Change Software License from LGPL v2.1 to LGPL v3.

--- in_tta.cpp
+++ in_tta.cpp
@@ -11,23 +11,24 @@
  */
 
  /*
- The ttaplugin-winamp project.
- Copyright (C) 2005-2026 Yamagata Fumihiro
+The ttaplugins-winamp project.
+Copyright (C) 2005-2026 Yamagata Fumihiro
 
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or any later version.
+This file is part of in_tta.
 
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
+in_tta is free software: you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, either
+version 3 of the License, or any later version.
 
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
+in_tta is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with enc_tta.
+If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #include <Shlwapi.h>
 #include <type_traits>
