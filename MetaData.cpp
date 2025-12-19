c060d3c6c9e47a0997a7232ee0b8f99a6309f06b
Yamagata Fumihiro <bunbun042000@gmail.com>
2025-12-19 18:00:20 +0900

    working commit.

--- MetaData.cpp
+++ MetaData.cpp
@@ -19,6 +19,12 @@ If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <agave/Metadata/svc_metatag.h>
+#include <Agave/Metadata/api_metadata.h>
+#include <Wasabi/bfc/dispatch.h>
+#include <Wasabi/api/service/api_service.h>
+#include <Wasabi/api/service/waservicefactory.h>
+#include <winamp/wa_ipc.h>
+
 #include <taglib/tag.h>
 #include <taglib/trueaudiofile.h>
 #include <taglib/id3v2tag.h>
@@ -29,12 +35,15 @@ If not, see <https://www.gnu.org/licenses/>.
 #include <iomanip>
 #include <strsafe.h>
 #include <map>
+#include <vector>
 
 #include "agaveCommon.h"
 #include "ID3v2TagExtension.h"
 
 static const size_t TAGNAME_LENGTH = 100;
 
+
+/*
 struct TagInfo
 {
 	unsigned long	Length;
@@ -53,12 +62,33 @@ struct TagInfo
 	std::wstring	BPM;
 	std::wstring    bitrate;
 };
+*/
 
-class TTA_MetaData : public svc_metaTag
+class metaTagFactory : public waServiceFactory
+{
+public:
+	virtual ~metaTagFactory();
+
+	FOURCC GetServiceType();
+	const char* GetServiceName();
+	GUID GetGUID();
+	void* GetInterface(int global_lock);
+	int SupportNonLockingInterface();
+	int ReleaseInterface(void* ifc);
+	const char* GetTestString();
+	int ServiceNotify(int msg, int param1, int param2);
+
+protected:
+	RECVS_DISPATCH;
+};
+
+static metaTagFactory MetaTagFactory;
+
+class TTA_metaTag : public svc_metaTag
 {
 	public:
-	TTA_MetaData();
-	virtual ~TTA_MetaData();
+	TTA_metaTag();
+	virtual ~TTA_metaTag();
 
 	static FOURCC getServiceType() { return svc_metaTag::SERVICETYPE; }
 	const wchar_t* getName();	// i.e. "ID3v2" or something
@@ -84,12 +114,37 @@ private:
 	std::wstring		m_FileName;
 	bool				m_isValidFile;
 	unsigned long		m_Length;
+	bool				m_isChanged;
 
+protected:
+	RECVS_DISPATCH;
 };
 
 // {50846701-71A9-40CF-9165-587D3A7DB325}
-static const GUID TTA_MetaData_GUID =
+static const GUID TTA_metaTag_GUID =
 { 0x50846701, 0x71a9, 0x40cf, { 0x91, 0x65, 0x58, 0x7d, 0x3a, 0x7d, 0xb3, 0x25 } };
+
+enum
+{
+	METATAG_LENGTH,
+	METATAG_FORMATINFORMATION,
+	METATAG_TYPE,
+	METATAG_FAMILY,
+	METATAG_LOSSLESS,
+	METATAG_TITLE,
+	METATAG_ARTIST,
+	METATAG_ALBUMARTIST,
+	METATAG_COMMENT,
+	METATAG_ALBUM,
+	METATAG_YEAR,
+	METATAG_GENRE,
+	METATAG_TRACK,
+	METATAG_COMPOSER,
+	METATAG_PUBLISHER,
+	METATAG_DISC,
+	METATAG_BPM,
+	METATAG_BITRATE,
+};
 
 static const wchar_t tagName[][TAGNAME_LENGTH] =
 {
@@ -113,21 +168,21 @@ static const wchar_t tagName[][TAGNAME_LENGTH] =
 	L"bitrate",
 };
 
-TTA_MetaData::TTA_MetaData() : svc_metaTag()
+TTA_metaTag::TTA_metaTag() : svc_metaTag()
 {
 	::InitializeCriticalSection(&m_CriticalSection);
 
 	FlushCache();
 }
 
-TTA_MetaData::~TTA_MetaData()
+TTA_metaTag::~TTA_metaTag()
 {
 	FlushCache();
 
 	::DeleteCriticalSection(&m_CriticalSection);
 }
 
-void TTA_MetaData::FlushCache()
+void TTA_metaTag::FlushCache()
 {
 	::EnterCriticalSection(&m_CriticalSection);
 
@@ -136,39 +191,40 @@ void TTA_MetaData::FlushCache()
 	m_FileName = L"";
 
 	m_isValidFile = false;
+	m_isChanged = false;
 
 	::LeaveCriticalSection(&m_CriticalSection);
 }
 
-const wchar_t* TTA_MetaData::getName()
+const wchar_t* TTA_metaTag::getName()
 {
 	return L"ID3v2";
 }
 
-GUID TTA_MetaData::getGUID()
+GUID TTA_metaTag::getGUID()
 {
-	return TTA_MetaData_GUID;
+	return TTA_metaTag_GUID;
 }
 
-int TTA_MetaData::getFlags()
+int TTA_metaTag::getFlags()
 {
-	return METATAG_FILE_INFO;
+	return static_cast<int>(METATAG_FILE_INFO);
 }
 
-int TTA_MetaData::isOurFile(const wchar_t* filename)
+int TTA_metaTag::isOurFile(const wchar_t* filename)
 {
 	if (extensionW(filename))
 	{
-		return ((_wcsicmp(extensionW(filename), L"tta") == 0) || (_wcsicmp(extensionW(filename), L"TTA") == 0)) ? 1 : 0;
+		return ((_wcsicmp(extensionW(filename), L"tta") == 0) || (_wcsicmp(extensionW(filename), L"TTA") == 0)) ? METATAG_SUCCESS : METATAG_FAILED;
 	}
 	else
 	{
 		// Do nothing
 	}
-	return 0;
+	return METATAG_FAILED;
 }
 
-int TTA_MetaData::metaTag_open(const wchar_t* filename)
+int TTA_metaTag::metaTag_open(const wchar_t* filename)
 {
 	::EnterCriticalSection(&m_CriticalSection);
 
@@ -181,7 +237,7 @@ int TTA_MetaData::metaTag_open(const wchar_t* filename)
 		if (!TTAFile.isValid())
 		{
 			::LeaveCriticalSection(&m_CriticalSection);
-			return 0;
+			return METATAG_FAILED;
 		}
 		else
 		{
@@ -223,42 +279,42 @@ int TTA_MetaData::metaTag_open(const wchar_t* filename)
 			<< L"kbit/s\nNum. of Chan.\t: " << TTAFile.audioProperties()->channels()
 			<< L"(" << channel_designation
 			<< L")\nLength\t\t: " << second.str();
-		m_Tag.insert(std::make_pair(L"formatinformation", ttainfo_temp.str()));
-		m_Tag.insert(std::make_pair(L"bitrate", std::to_wstring(static_cast<long long>(TTAFile.audioProperties()->bitrate()))));
-		m_Tag.insert(std::make_pair(L"type", std::wstring(L"0")));
-		m_Tag.insert(std::make_pair(L"family", std::wstring(L"The True Audio File")));
-		m_Tag.insert(std::make_pair(L"lossless", std::wstring(L"1")));
+		m_Tag.insert(std::make_pair(tagName[METATAG_FORMATINFORMATION], ttainfo_temp.str()));
+		m_Tag.insert(std::make_pair(tagName[METATAG_BITRATE], std::to_wstring(static_cast<long long>(TTAFile.audioProperties()->bitrate()))));
+		m_Tag.insert(std::make_pair(tagName[METATAG_TYPE], std::wstring(L"0")));
+		m_Tag.insert(std::make_pair(tagName[METATAG_FAMILY], std::wstring(L"The True Audio File")));
+		m_Tag.insert(std::make_pair(tagName[METATAG_LOSSLESS], std::wstring(L"1")));
 
 		if (nullptr != TTAFile.ID3v2Tag())
 		{
 			ID3v2TagExtension* Tag_ex = static_cast<ID3v2TagExtension*>(TTAFile.ID3v2Tag());
-			m_Tag.insert(std::make_pair(L"title", Tag_ex->title().toCWString()));
-			m_Tag.insert(std::make_pair(L"artist", Tag_ex->artist().toCWString()));
-			m_Tag.insert(std::make_pair(L"album", Tag_ex->album().toCWString()));
-			m_Tag.insert(std::make_pair(L"comment", Tag_ex->comment().toCWString()));
-			m_Tag.insert(std::make_pair(L"genre", Tag_ex->genre().toCWString()));
-			m_Tag.insert(std::make_pair(L"year", Tag_ex->stringYear().toCWString()));
-			m_Tag.insert(std::make_pair(L"track", Tag_ex->stringTrack().toCWString()));
-			m_Tag.insert(std::make_pair(L"albumartist", Tag_ex->albumArtist().toCWString()));
-			m_Tag.insert(std::make_pair(L"composer", Tag_ex->composers().toCWString()));
-			m_Tag.insert(std::make_pair(L"publisher", Tag_ex->publisher().toCWString()));
-			m_Tag.insert(std::make_pair(L"disc", Tag_ex->disc().toCWString()));
-			m_Tag.insert(std::make_pair(L"bpm", Tag_ex->BPM().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_TITLE], Tag_ex->title().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_ARTIST], Tag_ex->artist().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_ALBUM], Tag_ex->album().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_COMMENT], Tag_ex->comment().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_GENRE], Tag_ex->genre().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_YEAR], Tag_ex->stringYear().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_TRACK], Tag_ex->stringTrack().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_ALBUMARTIST], Tag_ex->albumArtist().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_COMPOSER], Tag_ex->composers().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_PUBLISHER], Tag_ex->publisher().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_DISC], Tag_ex->disc().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_BPM], Tag_ex->BPM().toCWString()));
 
 		}
 		else if (nullptr != TTAFile.ID3v1Tag())
 		{
 			std::wstringstream temp_year;
 			std::wstringstream temp_track;
-			m_Tag.insert(std::make_pair(L"title", TTAFile.ID3v1Tag()->title().toCWString()));
-			m_Tag.insert(std::make_pair(L"artist", TTAFile.ID3v1Tag()->artist().toCWString()));
-			m_Tag.insert(std::make_pair(L"comment", TTAFile.ID3v1Tag()->comment().toCWString()));
-			m_Tag.insert(std::make_pair(L"album", TTAFile.ID3v1Tag()->album().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_TITLE], TTAFile.ID3v1Tag()->title().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_ARTIST], TTAFile.ID3v1Tag()->artist().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_COMMENT], TTAFile.ID3v1Tag()->comment().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_ALBUM], TTAFile.ID3v1Tag()->album().toCWString()));
 			temp_year << TTAFile.ID3v1Tag()->year();
-			m_Tag.insert(std::make_pair(L"year", temp_year.str()));
-			m_Tag.insert(std::make_pair(L"genre", TTAFile.ID3v1Tag()->genre().toCWString()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_YEAR], temp_year.str()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_GENRE], TTAFile.ID3v1Tag()->genre().toCWString()));
 			temp_track << TTAFile.ID3v1Tag()->track();
-			m_Tag.insert(std::make_pair(L"track", temp_track.str()));
+			m_Tag.insert(std::make_pair(tagName[METATAG_TRACK], temp_track.str()));
 
 		}
 		else
@@ -274,21 +330,89 @@ int TTA_MetaData::metaTag_open(const wchar_t* filename)
 
 	::LeaveCriticalSection(&m_CriticalSection);
 
-	return 1;
+	return METATAG_SUCCESS;
 
 }
 
-void TTA_MetaData::metaTag_close()
+void TTA_metaTag::metaTag_close()
 {
-	// Do nothing Now.
+	if (m_isChanged)
+	{
+		::EnterCriticalSection(&m_CriticalSection);
+
+		if (m_FileName.empty())
+		{
+			::LeaveCriticalSection(&m_CriticalSection);
+			return;
+		}
+		else
+		{
+			TagLib::TrueAudio::File TTAFile(m_FileName.c_str());
+
+			if (!TTAFile.isValid())
+			{
+				::LeaveCriticalSection(&m_CriticalSection);
+				return;
+			}
+			else
+			{
+				// Do nothing
+			}
+
+			if (nullptr != TTAFile.ID3v2Tag(true))
+			{
+				ID3v2TagExtension* Tag_ex = static_cast<ID3v2TagExtension*>(TTAFile.ID3v2Tag());
+				Tag_ex->setTitle(m_Tag.at(tagName[METATAG_TITLE]));
+				Tag_ex->setArtist(m_Tag.at(tagName[METATAG_ARTIST]));
+				Tag_ex->setAlbum(m_Tag.at(tagName[METATAG_ALBUM]));
+				Tag_ex->setComment(m_Tag.at(tagName[METATAG_COMMENT]));
+				Tag_ex->setGenre(m_Tag.at(tagName[METATAG_GENRE]));
+				Tag_ex->setStringYear(m_Tag.at(tagName[METATAG_YEAR]));
+				Tag_ex->setStringTrack(m_Tag.at(tagName[METATAG_TRACK]));
+				Tag_ex->setAlbumArtist(m_Tag.at(tagName[METATAG_ALBUMARTIST]));
+				Tag_ex->setComposers(m_Tag.at(tagName[METATAG_COMPOSER]));
+				Tag_ex->setPublisher(m_Tag.at(tagName[METATAG_PUBLISHER]));
+				Tag_ex->setDisc(m_Tag.at(tagName[METATAG_DISC]));
+				Tag_ex->setBPM(m_Tag.at(tagName[METATAG_BPM]));
+
+			}
+			else if (nullptr != TTAFile.ID3v1Tag(true))
+			{
+				TTAFile.ID3v1Tag()->setTitle(m_Tag.at(tagName[METATAG_TITLE]));
+				TTAFile.ID3v1Tag()->setArtist(m_Tag.at(tagName[METATAG_ARTIST]));
+				TTAFile.ID3v1Tag()->setAlbum(m_Tag.at(tagName[METATAG_ALBUM]));
+				TTAFile.ID3v1Tag()->setComment(m_Tag.at(tagName[METATAG_COMMENT]));
+				TTAFile.ID3v1Tag()->setYear(static_cast<unsigned int>(_wtoi(m_Tag.at(tagName[METATAG_YEAR]).c_str())));
+				TTAFile.ID3v1Tag()->setTrack(static_cast<unsigned int>(_wtoi(m_Tag.at(tagName[METATAG_TRACK]).c_str())));
+				TTAFile.ID3v1Tag()->setGenre(m_Tag.at(tagName[METATAG_GENRE]));
+			}
+			else
+			{
+				// Do nothing.
+			}
+			TTAFile.save();
+		}
+		::LeaveCriticalSection(&m_CriticalSection);
+	}
+	else
+	{
+		// Do nothing
+	}
+
+	FlushCache();
+	m_Tag.~map();
+
+	::DeleteCriticalSection(&m_CriticalSection);
+
+	return;
 }
 
-const wchar_t* TTA_MetaData::enumSupportedTag(int n, int* datatype)
+const wchar_t* TTA_metaTag::enumSupportedTag(int n, int* datatype)
 {
 	return tagName[0];
 }
 
-int TTA_MetaData::getTagSize(const wchar_t* tag, size_t* sizeBytes)
+int TTA_metaTag::getTagSize(const wchar_t* tag, size_t* sizeBytes)
 {
 	if (m_Tag.contains(tag))
 	{
@@ -302,7 +426,7 @@ int TTA_MetaData::getTagSize(const wchar_t* tag, size_t* sizeBytes)
 	return 1;
 }
 
-int TTA_MetaData::getMetaData(const wchar_t* tag, uint8_t* buf, int buflenBytes, int datatype)
+int TTA_metaTag::getMetaData(const wchar_t* tag, uint8_t* buf, int buflenBytes, int datatype)
 {
 	if (m_Tag.contains(tag))
 	{
@@ -310,12 +434,89 @@ int TTA_MetaData::getMetaData(const wchar_t* tag, uint8_t* buf, int buflenBytes,
 	}
 	else
 	{
-		return 0;
+		return METATAG_FAILED;
 	}
+	return METATAG_SUCCESS;
+}
+
+int TTA_metaTag::setMetaData(const wchar_t* tag, const uint8_t* buf, int buflenBytes, int datatype)
+{
+	m_isChanged = true;
+	m_Tag[tag] = reinterpret_cast<const wchar_t *>(buf);
+	return METATAG_SUCCESS;
+}
+
+#define CBCLASS TTA_metaTag
+START_DISPATCH;
+CB(SVC_METATAG_GETNAME, getName);
+CB(SVC_METATAG_GETGUID, getGUID);
+CB(SVC_METATAG_GETFLAGS, getFlags);
+CB(SVC_METATAG_ISOURFILE, isOurFile);
+CB(SVC_METATAG_OPEN, metaTag_open);
+CB(SVC_METATAG_CLOSE, metaTag_close);
+CB(SVC_METATAG_ENUMTAGS, enumSupportedTag);
+CB(SVC_METATAG_GETTAGSIZE, getTagSize);
+CB(SVC_METATAG_GETMETADATA, getMetaData);
+CB(SVC_METATAG_SETMETADATA, setMetaData);
+END_DISPATCH;
+#undef CBCLASS
+
+static TTA_metaTag metatag;
+
+
+metaTagFactory::~metaTagFactory()
+{
+}
+
+FOURCC metaTagFactory::GetServiceType()
+{
+	return metatag.getServiceType();
+}
+
+const char* metaTagFactory::GetServiceName()
+{
+	return "TTA Meta Tag Provider";
+}
+
+GUID metaTagFactory::GetGUID()
+{
+	return TTA_metaTag_GUID;
+}
+
+void* metaTagFactory::GetInterface(int global_lock)
+{
+	return &metatag;
+}
+
+int metaTagFactory::SupportNonLockingInterface()
+{
 	return 1;
 }
 
-int TTA_MetaData::setMetaData(const wchar_t* tag, const uint8_t* buf, int buflenBytes, int datatype)
+int metaTagFactory::ReleaseInterface(void* ifc)
 {
-	m_Tag[tag] = reinterpret_cast<const wchar_t *>(buf);
+	return 1;
 }
+
+const char* metaTagFactory::GetTestString()
+{
+	return 0;
+}
+
+int metaTagFactory::ServiceNotify(int msg, int param1, int param2)
+{
+	return 1;
+}
+
+#define CBCLASS metaTagFactory
+START_DISPATCH;
+CB(WASERVICEFACTORY_GETSERVICETYPE, GetServiceType)
+CB(WASERVICEFACTORY_GETSERVICENAME, GetServiceName)
+CB(WASERVICEFACTORY_GETGUID, GetGUID)
+CB(WASERVICEFACTORY_GETINTERFACE, GetInterface)
+CB(WASERVICEFACTORY_SUPPORTNONLOCKINGGETINTERFACE, SupportNonLockingInterface)
+CB(WASERVICEFACTORY_RELEASEINTERFACE, ReleaseInterface)
+CB(WASERVICEFACTORY_GETTESTSTRING, GetTestString)
+CB(WASERVICEFACTORY_SERVICENOTIFY, ServiceNotify)
+END_DISPATCH;
+#undef CBCLASS
